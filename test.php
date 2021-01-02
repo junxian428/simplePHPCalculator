d37cef8f583f0f0829ef979154590e15565e50f9
junxian428 <58724748+junxian428@users.noreply.github.com>
2021-01-02 19:40:59 +0000

    Add files via upload

---
+++ test.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+  </head>
+  
+
+  <body>
+   <form action='test.php' method='get'>
+ <input type='number' name='num1'> 
+<br>
+<input type='number' name='num2'> 
+ <input type ='submit'>
+   
+</form>
+ANSWER:
+<?php
+
+echo $_GET["num1"] + $_GET['num2'];
+
+?>
+
+
+  </body>
+</html>
